c26aa8a4d9ba597095044d5372c0279887bba4ee
Amardeep Singh <amardeepsingh2602@gmail.com>
2025-12-01 13:57:05 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-# https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://portfolio-backend-9ekb.onrender.com",                       // ✅ important for Vercel
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),   // dev-only
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
